418e55e79ec0bd8a2b13642709cc43c36b569c86
Jordan Mor <jordanmor@gmail.com>
2018-08-11 14:27:37 -0400

    Create GameUI component

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict"
+
 $('body').prepend(`<div class="screen screen-start" id="start">
                     <header>
                       <h1>Tic Tac Toe</h1>
@@ -27,8 +29,65 @@ $('#board').after(`<div id="modal" class="modal">
                   </div>`
 );
 
-$('#board').hide();
-// $('#finish').addClass('screen-win-one');
-$('#finish').hide();
-$('#modal').hide();
-// $('#start').hide();
+/*=============-=============-=============-=============
+                        COMPONENTS
+===============-=============-=============-===========*/
+
+// -- GAME UI COMPONENT -- //
+
+class GameUI {
+  constructor() {
+    this.$buttons = $('.button');
+    this.$board = $('#board');
+    this.$boxes = $('.box');
+    this.$start = $('#start');
+    this.$finish = $('#finish');
+    this.$modal = $('#modal');
+
+    this.$buttons.on('click', e => this.handleBtnClick(e) );
+    $('.close').on('click', () => this.closeModal() );
+  }
+
+  init() {
+    this.$board.hide();
+    this.$finish.hide();
+    this.$modal.hide();
+  }
+
+  newGame() {
+    this.resetGame();
+    this.$finish.hide();
+    this.$start.show();
+  }
+
+  startGame() {
+    this.$modal.hide();
+    this.$start.hide();
+    this.$board.show();
+  }
+
+  handleBtnClick(e) {
+    const buttonText = e.target.textContent;
+    
+    switch(buttonText) {
+      case '1 player':
+        this.$modal.show();
+        break;
+      case '2 players':
+        this.$modal.show();
+        break;
+      case 'Start Game':
+        this.startGame();  
+        break;
+      case 'New Game':
+        this.newGame();
+    }
+  }
+
+  closeModal() {
+    this.$modal.hide();
+  }
+}
+
+const game = new GameUI();
+game.init();
